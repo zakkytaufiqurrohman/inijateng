efaec332b4a5faa6d4b9ef309df32ef95309e132
zakky <zakky@trisip.co>
2022-02-10 20:09:24 +0700

    custome email body reset password

---
+++ app/Notifications/PasswordReset.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordReset extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+        ->greeting('Haai!')
+        ->line('Silahlkan klik tombol di bawah ini untuk reset password anda') // Here are the lines you can safely override
+        ->action('Reset Password', url('password/reset', $this->token))
+        ->line('Abaikan pesan ini jika anda tidak tidak ingin reset password');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
