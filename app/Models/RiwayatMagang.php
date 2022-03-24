ad1b7e063b1c1091926bbcc03e6dffb345a04bc6
KhaiZulfa18 <khaizulfa18@gmail.com>
2022-03-24 22:09:35 +0700

    riwayat magang

---
+++ app/Models/RiwayatMagang.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RiwayatMagang extends Model
+{
+    use HasFactory;
+
+    protected $table = "riwayat_magang";
+
+    protected $fillable = [
+        'penerima_magang',
+        'tempat_magang',
+        'wilayah_kerja',
+        'masa_magang',
+        'tgl_no_surat',
+        'user_id'
+    ];
+}
