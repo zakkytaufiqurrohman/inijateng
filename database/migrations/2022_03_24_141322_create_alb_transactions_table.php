1bc88e38a02137467b96f161754ef79abc163e62
zakky <zakky@trisip.co>
2022-03-25 21:46:45 +0700

    register alb

--- database/migrations/2022_03_24_141322_create_alb_transactions_table.php
+++ database/migrations/2022_03_24_141322_create_alb_transactions_table.php
@@ -15,6 +15,29 @@ class CreateAlbTransactionsTable extends Migration
     {
         Schema::create('alb_transactions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('alb_id');
+            $table->string('kode');
+            $table->string('nama');
+            $table->string('nik',20);
+            $table->string('email',100);
+            $table->string('wa',20);
+            $table->enum('bendahara_status',[0,1]);
+            $table->enum('verifikator_status',[0,1]);
+            $table->string('tempat_lahir');//hanya kabupaten
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->string('provinsi');
+            $table->string('kota',20);
+            $table->string('s1',100);
+            $table->string('tahun_s1',5);
+            $table->string('s2',100);
+            $table->string('tahun_s2',5);
+            $table->string('foto',255);
+            $table->string('suket',255);
+            $table->string('ijazah_s1',255);
+            $table->string('ijazah_s2',255);
+            $table->string('ktp',255);
+            $table->string('bukti_bayar',255);
             $table->timestamps();
         });
     }
