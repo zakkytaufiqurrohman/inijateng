f8a4b05a6ecbc1b3c9ba0d0817b46b0af9ec7fc1
KhaiZulfa18 <khaizulfa18@gmail.com>
2022-03-27 22:32:47 +0700

    fix

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,7 +8,7 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-           
+            @role('admin')
             <li class="nav-item dropdown {{ (request()->is('user*','role*','permission*','master_data*')) ? 'active' : '' }}">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Master</span></a>
                 <ul class="dropdown-menu">
@@ -23,7 +23,7 @@
                    
                 </ul>
             </li>
-          
+            @endrole
             @role('notaris')
             <li class="nav-item {{ (request()->is('notaris*')) ? 'active' : '' }}">
                 <a href="{{ route('notaris.data_diri') }}" class="nav-link"><i class="fas fa-user-circle"></i><span>Data Diri</span></a>
