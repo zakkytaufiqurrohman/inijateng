1f7ff0e4667c15b45a69ee58b9b3257cb614dabd
KhaiZulfa18 <khaizulfa18@gmail.com>
2022-04-10 14:20:21 +0700

    update view alb

--- database/migrations/2022_04_03_165124_create_table_view_alb.php
+++ database/migrations/2022_04_03_165124_create_table_view_alb.php
@@ -14,13 +14,45 @@ class CreateTableViewAlb extends Migration
     public function up()
     {
         $query = <<<SQL
-            CREATE VIEW `v_alb` AS
+            CREATE OR REPLACE VIEW `v_alb` AS
             SELECT u.*, 
-                da.id as da_id, da.bukti_terdaftar, da.ijazah_s1, da.ijazah_s2, da.no_alb, da.s1, da.s2, da.tgl_lulus_s1, da.tgl_lulus_s2, 
-                dba.id as dba_id, dba.ktp, dba.pengantar_magang, dba.suket_pengda, dba.ttmb1, dba.ttmb2, dba.ttmb3, dba.ttmb4,
-                rm.id as rm_id, rm.penerima_magang, rm.masa_magang, rm.tempat_magang, rm.tgl_no_surat, rm.magang_ke,
-                rt.id as rt_id, rt.pengwil, rt.nilai, rt.tgl_nomor, rt.materi, rt.tgl_pelaksanaan, rt.magang_ke as ttmb_magang_ke
+                da.id as da_id, 
+                da.bukti_terdaftar, 
+                da.ijazah_s1, 
+                da.ijazah_s2, 
+                da.no_alb, 
+                da.s1,
+                da.s2, 
+                da.tgl_lulus_s1, 
+                da.tgl_lulus_s2, 
+                dba.id as dba_id, 
+                dba.ktp,
+                dba.pengantar_magang, 
+                dba.suket_pengda, 
+                dba.ttmb1, 
+                dba.ttmb2,
+                dba.ttmb3, 
+                dba.ttmb4,
+                rm.id as rm_id, 
+                rm.penerima_magang, 
+                rm.masa_magang, 
+                rm.tempat_magang, 
+                rm.tgl_no_surat, 
+                rm.magang_ke,
+                rt.id as rt_id, 
+                rt.pengwil, 
+                rt.nilai, 
+                rt.tgl_nomor, 
+                rt.materi, 
+                rt.tgl_pelaksanaan,
+                rt.magang_ke as ttmb_magang_ke,
+                t.`name` as tempat_lahir_name,
+                ic.`name` as kota_name,
+                ip.`name` as provinsi_name
                 FROM users u 
+                left join indonesia_cities ic on ic.id = u.kota
+                left join indonesia_provinces ip on ip.id = u.provinsi
+                left join indonesia_cities t on t.id = u.tempat_lahir
                 LEFT JOIN detail_alb da on da.user_id=u.id
                 LEFT JOIN detail_berkas_alb dba on dba.user_id=u.id
                 LEFT JOIN (
