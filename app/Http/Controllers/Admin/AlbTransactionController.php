9e611e3d3796574c13f1c0700003edac8a7dc893
zakky <zakky@trisip.co>
2022-04-15 14:46:58 +0700

    verif alb

--- app/Http/Controllers/Admin/AlbTransactionController.php
+++ app/Http/Controllers/Admin/AlbTransactionController.php
@@ -77,4 +77,61 @@ class AlbTransactionController extends Controller
         $data->save();
         return response()->json(['status' => 'success', 'message' => 'Berhasil Merubah Data']);
     }
+
+    public function verifikasiIndex($id)
+    {
+        return view('Admin.alb.verifikasi',compact('id'));
+    }
+
+    public function verifikasi(Request $request){
+        
+        //tampilakan data dengan event  yang aktif
+        $AlbEvent = Alb::where('status','1')->first();
+        $data = AlbTransaction::with('provincies','kotas')->where('verifikator_status',$request->status)->where('alb_id',$AlbEvent->id)->get();
+        return DataTables::of($data)
+            ->addColumn('action', function ($data) {
+                $link = route('verifikasi_alb.show',$data->id);
+                $action = '';
+                $action .= "<a href='$link' class='btn btn-icon btn-primary'><i class='fa fa-edit'></i></a>&nbsp;";
+
+                return $action;
+            })
+            ->addColumn('nik',function ($data) {
+                return $data->nik;
+            })
+            ->addColumn('wa', function ($data) {
+                return $data->wa;
+            })
+            ->addColumn('name', function ($data) {
+                return $data->nama;
+            })
+            ->addColumn('provinsi', function ($data) {
+                return $data->provincies->name;
+            })
+            ->addColumn('kota', function ($data) {
+                return $data->kotas->name;
+            })
+            ->escapeColumns([])
+            ->addIndexColumn()
+            ->make(true);
+    }
+
+    public function verifikasi_show($id)
+    {
+        $alb_event = Alb::where('status','1')->first();
+        $data = AlbTransaction::with('provincies','kotas')->where('alb_id',$alb_event->id)->where('id',$id)->first();
+        $data['link_group'] = $alb_event->link_group;
+
+        return view('Admin.alb.verifikasi_verified',compact('alb_event','data'));
+    }
+
+    public function verifikasi_validasi(Request $request)
+    {
+        $status = (($request->status==0)&&$request->status<>1) ? '1' : '0';
+        $data = AlbTransaction::where('id',$request->id)->first();
+        $data->verifikator_status = $status;
+        $data->save();
+
+        return response()->json(['status' => 'success', 'message' => 'Berhasil Merubah Status']);
+    }
 }
