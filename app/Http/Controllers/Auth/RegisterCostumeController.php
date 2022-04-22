88a9d802a4b5269b22771948ecaf83e53d5d594a
kelvin <kelvinpradana11@gmail.com>
2022-04-22 21:17:54 +0700

    user registration

--- app/Http/Controllers/Auth/RegisterCostumeController.php
+++ app/Http/Controllers/Auth/RegisterCostumeController.php
@@ -20,28 +20,51 @@ class RegisterCostumeController extends Controller
         $provinces = $dependant->provinces();
         // $provinces = $dependant::provinces();
 
-        return view('auth.register2',compact('provinces'));
-    } 
+        return view('auth.register2', compact('provinces'));
+    }
 
     public function check_user(Request $request)
     {
         $validated = $request->validate([
             'nik' => 'required'
-        ],[
+        ], [
             'nik.required' => 'NIK Masih Kosong'
         ]);
 
         try {
-            $user = User::where('nik',$request->nik)->first();
-            if(!$user){
+            $user = User::where('nik', $request->nik)->first();
+            if (!$user) {
                 return response()->json(['status' => 'error', 'message' => 'NIK Belum Terdaftar, harap hubungi admin']);
             }
             $user['is_check'] = (!empty($user->password)) ? 1 : 0;
-            
+
             return response()->json(['status' => 'success', 'message' => $user]);
-        } catch(Exception $e){
+        } catch (Exception $e) {
             return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
         }
+    }
+
+    public function reg_account()
+    {
+        return view('auth.accountRegister');
+    }
+
+    public function store_account(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users,name',
+            'nik' => 'required|unique:users,nik|digits:16',
+            'email' => 'required|unique:users,email',
+        ]);
+
+        $data = User::create([
+            'name' => $request->input('name'),
+            'nik' => $request->input('nik'),
+            'email' => $request->input('email'),
+            'status_anggota' => 'alb',
+        ]);
+        $data->assignRole('alb');
+        return redirect('/register')->with('success', 'Data Berhasil Didaftarkan! Masukan NIK dan Lanjutkan Membuat Akun');
     }
 
     public function register(Request $request)
@@ -51,21 +74,21 @@ class RegisterCostumeController extends Controller
             'email' => 'required',
             'password' => 'required|min:6',
             'password_confirm' => 'required_with:password|same:password|min:6'
-        ],[
+        ], [
             'user_id.required' => 'ID tidak ada',
             'email.required' => 'Email tidak boleh kosong',
             'password.required' => 'Password tidak boleh kosong',
         ]);
 
         DB::beginTransaction();
-        try{
+        try {
 
-            $user = User::where('id',$request->user_id)->first();
+            $user = User::where('id', $request->user_id)->first();
             $update = $user->update([
                 'email' => $request->email,
                 'password' => bcrypt($request->password)
             ]);
-            
+
             $request->merge([
                 'nik' => $user->nik
             ]);
@@ -79,7 +102,7 @@ class RegisterCostumeController extends Controller
             $login->login($request);
 
             return response()->json(['status' => 'success', 'message' => 'Pendaftaran Berhasil!']);
-        } catch(Exception $e){
+        } catch (Exception $e) {
 
             DB::rollback();
 
---
+++ resources/views/auth/accountRegister.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('node_modules/bootstrap-social/bootstrap-social.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('node_modules/izitoast/dist/css/iziToast.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('node_modules/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('node_modules/select2/dist/css/select2.min.css') }}">
+    <title>Register &mdash; INI Jateng</title>
+</head>
+
+<body>
+
+    <div class="container mt-3">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <div class="card shadow">
+                    <div class="login-brand">
+                        <img src="{{ asset('assets/img/logo.png') }}" alt="logo" width="70" class="shadow-light rounded-circle">
+                    </div>
+                    <div class="card-body">
+                        <form action="/register/inijateng/submit" method="POST" enctype="multipart/form-data" class="m-sm-3">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Nama Lengkap</label>
+                                <input type="text" class="form-control" id="name" name="name" aria-describedby="name">
+                                @error('name')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                                <div id="name" class="form-text">Pastikan Menulis Nama Dengan Gelar Yang Bendar</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="nik" class="form-label">NIK</label>
+                                <input type="text" class="form-control" id="nik" name="nik" aria-describedby="nik">
+                                <div id="nik" class="form-text">Nomor Induk Kependudukan</div>
+                                @error('nik')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" name="email" id="email" aria-describedby="email">
+                                @error('email')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-primary">Daftar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer">
+                        <a href="https://wa.me/6281240417949" target="_blank">Need Help?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
